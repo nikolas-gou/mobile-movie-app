d5e95f91e7ff28782a7c1ff9818539b53dabcb82
nikolas-gou <nikolasgou23@gmail.com>
2026-03-29 22:19:51 +0300

    Connecting DB, Creating search count mechanism

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -5,6 +5,7 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 
 import MovieDisplayCard from "@/components/MovieCard";
@@ -30,6 +31,11 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
+
+                // Call updateSearchCount only if there are results
+                if (movies?.length! > 0 && movies?.[0]) {
+                    await updateSearchCount(searchQuery, movies[0]);
+                }
             } else {
                 reset();
             }
