e3cae147cb61eb041c4e10f75f33aee7381be014
nikolas-gou <nikolasgou23@gmail.com>
2026-03-30 16:20:56 +0300

    Fixing Bug on loadMovies and updateCont

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -5,11 +5,11 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 
 import { fetchMovies } from "@/services/api";
-import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 
 import MovieDisplayCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import { updateSearchCount } from "@/services/appwrite";
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = useState("");
@@ -31,11 +31,6 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
-
-                // Call updateSearchCount only if there are results
-                if (movies?.length! > 0 && movies?.[0]) {
-                    await updateSearchCount(searchQuery, movies[0]);
-                }
             } else {
                 reset();
             }
@@ -43,6 +38,12 @@ const Search = () => {
 
         return () => clearTimeout(timeoutId);
     }, [searchQuery]);
+
+    useEffect(() => {
+        if (movies?.length! > 0 && movies?.[0]) {
+            updateSearchCount(searchQuery, movies[0]);
+        }
+    }, [movies]);
 
     return (
         <View className="flex-1 bg-primary">
